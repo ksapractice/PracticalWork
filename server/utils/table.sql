427ddca31bbe908ec7a90bc50dcba127e1719059
Vombit <45901675+Vombit@users.noreply.github.com>
2021-05-31 04:16:11 +0500

    Add files via upload

---
+++ server/utils/table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE devicemovingview (
+  Stamp text,
+  Serial text,
+  Production_date text,
+  Objectt text,
+  Coming_date text,
+  Exit_date text,
+  Sender text,
+  Recipient text
+) 
